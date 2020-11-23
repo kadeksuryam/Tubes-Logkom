74c3cb8af7f5af1dc83835ce1d2a0b2b69c0d83b
reynull20 <muhammadreyhan2000@gmail.com>
2020-11-23 13:31:38 +0700

    update map.pl

---
+++ map.pl
@@ -0,0 +1,27 @@
+/* Posisi pemain disimpan dengan koordinat x,y dimana x >= 0 dan y >= 0 */
+:- dynamic(playerCoor/2).
+
+playerCoor(20,-20).
+initmap :- 
+	random(1,20,X),
+	random(1,20,Y),
+	asserta(playerCoor(X,Y)).
+
+/* ukuran map 20x20 */
+pagar(0,_).
+pagar(_,0).
+pagar(21,_).
+pagar(_,-21).
+
+/* Basis */
+endmap(21,-21):- 
+	write('#'),nl.
+/* Rekurens */
+wrtmap(X,Y):- 
+	endmap(X,Y);
+	pagar(X,Y),(X =\= 21),write('#'),(Z is X+1),wrtmap(Z,Y);
+	pagar(X,Y),(X == 21),write('#'),nl,(XX is 0),(YY is Y-1),wrtmap(XX,YY);
+	playerCoor(X,Y),write('P'),(Z is X+1),wrtmap(Z,Y);
+	write('-'),(Z is X+1),wrtmap(Z,Y).
+	
+map :- wrtmap(0,0),!.
